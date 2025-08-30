c135d88e60daae718483da4e7885d2ad24cad7c9
Youssef chakir <76889503+Yousssefch@users.noreply.github.com>
2025-08-29 22:06:32 -0400

    basic hud re design

--- Scripts/UI/Hud.cs
+++ Scripts/UI/Hud.cs
@@ -4,13 +4,9 @@ using System.Diagnostics;
 
 public partial class Hud : CanvasLayer
 {
-	private Label actName;
 	private Label coinsLabel;
 	private Label timerLabel;
-	private TextureRect weatherGUI;
 	private Timer timer;
-	private enum weatherState { Sunny, Night }
-	[Export] public string ActName = "Act 1";
 	public static int numberOfCoins = 0;
 	private int minutes = 0;
 	private int seconds = 0;
@@ -20,22 +16,25 @@ public partial class Hud : CanvasLayer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		actName = GetNode("VBoxContainer").GetNode<Label>("ActName");
-		coinsLabel = GetNode("VBoxContainer").GetNode<Label>("Coins");
-		timerLabel = GetNode("VBoxContainer").GetNode<Label>("TimerLabel");
+		coinsLabel = GetNode("VBoxContainer").GetNode("HBoxContainer3").GetNode<Label>("CoinsValue");
+		timerLabel = GetNode("VBoxContainer").GetNode("HBoxContainer2").GetNode<Label>("TimerValue");
 		timer = GetNode<Timer>("Timer");
-		actName.Text = ActName;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		coinsLabel.Text = "Coins: " + numberOfCoins.ToString();
+		coinsLabel.Text = numberOfCoins.ToString();
 
+		updateTimer();
+	}
+
+	private void updateTimer()
+	{
 		string minutesString = minutes < 10 ? "0" + minutes.ToString() : minutes.ToString();
 		string secondsString = seconds < 10 ? "0" + seconds.ToString() : seconds.ToString();
 		string miliSecondsString = miliSeconds < 10 ? "0" + miliSeconds.ToString() : miliSeconds.ToString();
-		timerLabel.Text = "Timer: " + minutesString + ":" + secondsString + ":" + miliSecondsString;
+		timerLabel.Text = minutesString + ":" + secondsString + ":" + miliSecondsString;
 	}
 
 	//Signals
@@ -44,7 +43,7 @@ public partial class Hud : CanvasLayer
 		//Update Timer
 		if (!(miliSeconds + 1 >= 60))
 		{
-			miliSeconds++;	
+			miliSeconds++;
 		}
 		else if (!(seconds + 1 >= 60))
 		{
